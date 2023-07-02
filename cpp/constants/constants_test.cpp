d7ed65bce99a39f9f1d9859c2ac284593b3284cb
ajarombek <ajarombek95@gmail.com>
2023-07-02 14:59:34 -0400

    test

--- cpp/constants/constants_test.cpp
+++ cpp/constants/constants_test.cpp
@@ -2,4 +2,24 @@
 // Author: Andrew Jarombek
 // Date: 7/2/2023
 
+#include "array.h"
+#include <gtest/gtest.h>
 
+class ArrayTest: public ::testing::Test {
+protected:
+    Array<int, 3> array;
+
+    void SetUp() override {
+        array = {1, 2, 3};
+    }
+};
+
+TEST_F(ArrayTest, GetSize) {
+    EXPECT_EQ(array.size(), 3);
+}
+
+// Run all the tests
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
