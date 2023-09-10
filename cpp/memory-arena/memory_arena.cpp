8087c52cb1770b05a82bef8aa2b6262990ad0082
ajarombek <ajarombek95@gmail.com>
2023-09-09 20:57:16 -0400

    memory arena code

--- cpp/memory-arena/memory_arena.cpp
+++ cpp/memory-arena/memory_arena.cpp
@@ -2,4 +2,28 @@
 // Author: Andrew Jarombek
 // Date: 9/9/2023
 
+#include "memory_arena.h"
 
+MemoryArena::MemoryArena(size_t size) {
+    arena = new char[size];
+    capacity = size;
+    offset = 0;
+}
+
+MemoryArena::~MemoryArena() {
+    delete[] arena;
+}
+
+void* MemoryArena::allocate(size_t size) {
+    if (offset + size > capacity) {
+        return nullptr;
+    }
+
+    void* ptr = arena + offset;
+    offset += capacity;
+    return ptr;
+}
+
+void MemoryArena::reset() {
+    offset = 0;
+}
--- cpp/memory-arena/memory_arena.h
+++ cpp/memory-arena/memory_arena.h
@@ -5,6 +5,19 @@
 #ifndef CONSTANTSTESTS_MEMORY_ARENA_H
 #define CONSTANTSTESTS_MEMORY_ARENA_H
 
+#include <cstddef>
 
+class MemoryArena {
+private:
+    char* arena;
+    size_t capacity;
+    size_t offset;
+public:
+    MemoryArena(size_t size);
+    ~MemoryArena();
+
+    void* allocate(size_t size);
+    void reset();
+};
 
 #endif //CONSTANTSTESTS_MEMORY_ARENA_H
